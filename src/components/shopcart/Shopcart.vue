f6c99e0f4c9fb177fdf4740db375515d9bef74ce
XiaoCai20180701 <421586311@qq.com>
2019-03-29 19:14:33 +0800

    initialize shopcart

---
+++ src/components/shopcart/Shopcart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    shopcart
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'shopcart',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
