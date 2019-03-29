798fb09ec60d5643a41fdf2fcf2d1794b23f37c7
XiaoCai20180701 <421586311@qq.com>
2019-03-29 19:27:49 +0800

    1.配置基本页面的路由 2.首先加载页面的路由重定向

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/home/Home'
+import Search from '@/components/search/Search'
+import Shopcart from '@/components/shopcart/Shopcart'
+import VIP from '@/components/vip/VIP'
+import NewsList from '@/components/news/NewsList'
 
 Vue.use(Router)
 
@@ -8,8 +12,32 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: {name: 'home'}
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search
+    },
+    {
+      path: '/shopcart',
+      name: 'shopcart',
+      component: Shopcart
+    },
+    {
+      path: '/VIP',
+      name: 'vip',
+      component: VIP
+    },
+    {
+      path: '/news/list',
+      name: 'news-list',
+      component: NewsList
     }
   ]
 })
