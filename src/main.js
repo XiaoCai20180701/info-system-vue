0a88b714c2da7360048720220fa46866b9beb186
XiaoCai20180701 <421586311@qq.com>
2019-03-29 19:18:11 +0800

    1.引入vant组件，并全局注册 2.引入axios,并配置axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Button, NavBar, Tabbar, TabbarItem, Swipe, SwipeItem, Lazyload, Row, Col, List, Card, Cell, CellGroup } from 'vant'
 
-Vue.config.productionTip = false
+// 配置axios
+import Axios from 'axios'
+// 配置公共URL
+Axios.defaults.baseURL = 'https://www.sinya.online/api/'
+Vue.prototype.$axios = Axios
+
+Vue.config.productionTip = false // 配置生产或开发环境
+
+Vue.use(Button)
+Vue.use(NavBar)
+Vue.use(Tabbar).use(TabbarItem)
+Vue.use(Swipe).use(SwipeItem)
+Vue.use(Lazyload)
+Vue.use(Row).use(Col)
+Vue.use(List)
+Vue.use(Card)
+Vue.use(Cell).use(CellGroup)
 
 /* eslint-disable no-new */
 new Vue({
