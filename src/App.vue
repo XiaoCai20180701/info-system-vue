ed88272730c3aa19c30a0ff94bffacd27e7a1c35
XiaoCai20180701 <421586311@qq.com>
2019-03-29 19:26:22 +0800

    搭建页面基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <van-nav-bar
+      title="信息系统"
+      fixed
+      class="nav-top-bar"
+    />
+    <router-view class="content" />
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o" to="/Home">首页</van-tabbar-item>
+      <van-tabbar-item icon="search" to="/VIP">会员</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" info="5" to="/Shopcart">购物车</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" to="/Search">搜索</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App',
+    data () {
+      return {
+        active: 0
+      }
+    },
+    watch: {
+    },
+    mounted: function () {
+      let name = this.$router.history.current.name
+      if (name === 'vip') {
+        this.active = 1
+      } else if (name === 'shopcart') {
+        this.active = 2
+      } else if (name === 'search') {
+        this.active = 3
+      } else {
+        this.active = 0
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .content {
+    margin-top: 46px;
+  }
+  .nav-top-bar {
+    background: #1989fa;
+  }
+  .nav-top-bar .van-nav-bar__title {
+    color: #fff;
+  }
 </style>
