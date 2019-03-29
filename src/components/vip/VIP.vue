6f1e5808201e3a4d4d92208646438b394dd42966
XiaoCai20180701 <421586311@qq.com>
2019-03-29 19:25:41 +0800

    initialize vip

---
+++ src/components/vip/VIP.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+   vip
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vip',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
