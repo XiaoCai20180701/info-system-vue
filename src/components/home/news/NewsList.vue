9dfb97a02cc0b847d54268e9272ca3c4ca1609a0
XiaoCai20180701 <421586311@qq.com>
2019-03-31 18:11:19 +0800

    新闻列表页面

--- src/components/home/news/NewsList.vue
+++ src/components/home/news/NewsList.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    <van-nav-bar
-      title="新闻列表"
-      left-text="返回"
-      left-arrow
-      @click-left="onClickLeft"
-      fixed
-      class="nav-bar-fixed"
-    />
+    <nav-bar title="新闻列表"/>
     <van-list
       v-model="loading"
       :finished="finished"
@@ -15,24 +8,26 @@
       :error.sync="error"
       error-text="请求失败，点击重新加载"
       @load="onLoad"
-      class="news-list"
+      class="news-list content-top"
     >
-      <div class="item"
-        v-for="(item, index) in list"
-        :key="item"
+      <div class="item content-bottom"
+        v-for="item in newsList"
+        :key="item.id"
         center
       >
         <van-row class="row">
-          <van-col :span="5">
-            <img :src="item.imageURL" :width="60" class="news-item-img"/>
-          </van-col>
-          <van-col :span="18" offset="1" >
-            <div class="custom-text">{{item.title + (index+1)}}</div>
-            <van-row class="news-item-subtitle">
-              <van-col :span="9">点击数：{{item.count}}</van-col>
-              <van-col :span="15">发表时间：{{item.time}}</van-col>
-            </van-row>
-          </van-col>
+          <router-link :to="{name: 'news-detail',query: {id: item.id}}">
+            <van-col :span="5">
+              <img :src="item.img_url" :width="60" class="news-item-img"/>
+            </van-col>
+            <van-col :span="18" offset="1" >
+              <div class="custom-text">{{item.title}}</div>
+              <van-row class="news-item-subtitle">
+                <van-col :span="9">点击数：{{item.click_count}}</van-col>
+                <van-col :span="15">发表时间：{{item.publish_time | dateFormat('YYYY-MM-DD') }}</van-col>
+              </van-row>
+            </van-col>
+          </router-link>
         </van-row>
       </div>
     </van-list>
@@ -46,16 +41,21 @@
       return {
         list: [
           {
+            id: 0,
             title: '新闻标题',
             count: 5,
-            time: '2019-01-08',
+            time: '20190109',
             imageURL: 'https://cdn4.buysellads.net/uu/1/3386/1525189943-38523.png'
           }
         ],
+        newsList: [],
         loading: false,
         error: false,
         finished: false
       }
+    },
+    created () {
+      this.getNewsList()
     },
     methods: {
       onLoad () {
@@ -63,12 +63,12 @@
         setTimeout(() => {
           for (let i = 0; i < 10; i++) {
             this.list.push({
+              id: i,
               title: '新闻标题',
               count: 9,
-              time: '2019-03-08',
+              time: '20190308',
               imageURL: 'https://cdn4.buysellads.net/uu/1/3386/1525189943-38523.png'
             })
-            console.log(this.list)
           }
           // 加载状态结束
           this.loading = false
@@ -79,12 +79,16 @@
           }
         }, 500)
         // 若数据加载失败，执行下面代码
-        // fetchSomeThing().catch(() => {
-        //  this.error = true;
         // })
       },
-      onClickLeft () {
-
+      getNewsList () {
+        this.$axios.get('getNewsList')
+          .then(res => {
+            let list = res.data
+            this.newsList = list
+            console.log(res)
+          })
+          .catch(err => console.log('新闻列表获取失败', err))
       }
     }
   }
@@ -111,9 +115,6 @@
   .row {
     width: 100%;
   }
-  .news-list {
-    margin-top: 98px;
-  }
   .news-item-img {
     vertical-align: top;
   }
@@ -122,8 +123,5 @@
     font-size: 12px;
     line-height: 20px;
     color: #1989fa;
-  }
-  .nav-bar-fixed {
-    margin-top: 46px;
   }
 </style>
